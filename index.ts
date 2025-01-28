013dc8feb0093e9d988de67b93540a7f0e42dced
Vitaliy <vitaliykorzenkoua@gmail.com>
2025-01-28 14:42:42 +0200

    prod
    prod

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 import express, { Request, Response } from 'express';
-import http from 'http'; // Используем HTTP вместо HTTPS для локальной разработки
+import https from 'https'; // Подключаем HTTPS вместо HTTP
+import fs from 'fs'; // Для чтения сертификатов
 import { Server } from 'socket.io';
 import socketHandler from './socketHandler';
 import teacherRouter from './router/teacherRouter';
-import cors from 'cors';
+import cors from 'cors'; 
 import dotenv from 'dotenv';
 import centralSocket from './centralSocket'; // Импорт WebSocket подключения
 
 dotenv.config();
 
 const app = express();
-// use env port or 4030
+//use env port or 4030
 const port = process.env.PORT || 4030;
+
+// Пути к сертификатам
+const keyPath = '/etc/letsencrypt/live/chatgovorika.chat/privkey.pem';
+const certPath = '/etc/letsencrypt/live/chatgovorika.chat/fullchain.pem';
+
+// Читаем сертификаты
+const httpsOptions = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath),
+};
 
 // Подключаем middleware для обработки JSON
 app.use(express.json());
@@ -27,8 +38,8 @@ app.use(cors({
 // Роутер
 app.use('/api', teacherRouter);
 
-// Создаём HTTP-сервер для локальной разработки
-const server = http.createServer(app);
+// Создаём HTTPS-сервер
+const server = https.createServer(httpsOptions, app);
 
 // Инициализируем Socket.IO
 const io = new Server(server, {
